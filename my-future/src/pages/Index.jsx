47a6d12e76f7de3ac70c55fcc01f62880fcce13d
K-A19 <kamiyeafolabi@hotmail.com>
2026-01-31 12:41:13 -0500

    commiting for sake

--- my-future/src/pages/Index.jsx
+++ my-future/src/pages/Index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './Index.css';
+import '../Index.css';
 
 const Index = () => {
   const handleLocationClick = (location) => {
